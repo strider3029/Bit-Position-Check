e1fd1de41550842fc0c30f8313f14dd1f3368ebc
B Gray <strider_3029@yahoo.com>
2015-04-28 21:48:43 +1000

    Initial Commit

---
+++ BitPositionCheck/BitPositionCheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace BitPositionCheck
+{
+    class BitPositionCheck
+    {
+        static int Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (null == line) continue;
+
+                    string[] numberBlocks = line.Split(',');
+
+                    int n  = Int32.Parse(numberBlocks[0].Trim());
+                    int p1 = Int32.Parse(numberBlocks[1].Trim());
+                    int p2 = Int32.Parse(numberBlocks[2].Trim());
+
+                    string binaryN = Convert.ToString(n, 2);
+
+                    Console.WriteLine((binaryN[binaryN.Length - p1] == binaryN[binaryN.Length - p2]) ? "true" : "false");
+                }
+            }
+
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+C# code sample
+
+CodeEval: Bit Positions
+URL: https://www.codeeval.com/open_challenges/19/
+
+Given a number n and two integers p1,p2 determine if the bits in position p1 and p2 are the same or not. Positions p1 and p2 are 1 based.
